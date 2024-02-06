8a539e3319762b449d79ec6c092ef71ef1258287
Kaj <kaj.robers@gmail.com>
2024-02-06 14:32:14 +0100

    Deel van H3

---
+++ H3.py
@@ -0,0 +1,18 @@
+
+#3.1
+vrienden = ["Henk", "Piet", "Jan", "Gerard", "Johan"]
+for x in vrienden:
+    print(x)
+
+#3.2
+for x in vrienden:
+    print("Hallo "+ x)
+
+#3.3
+fav_food = ["Pizza", "Pasta", "Wafels", "Nasi"]
+print("Mijn allerliefste eten is: " + fav_food[1])
+print("Daarna komt " + fav_food[0])
+print("Dan komt " + fav_food[3])
+print("Maar ik houd niet van " + fav_food[2])
+
+
