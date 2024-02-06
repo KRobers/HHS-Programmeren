7dcf9f32a5e85400c91ece38f97059fcf5b5a040
Kaj <kaj.robers@gmail.com>
2024-02-06 17:03:17 +0100

    H6 opdrachten deels af

---
+++ H6.py
@@ -0,0 +1,27 @@
+#6.1
+print("\n 6.1 \n")
+
+kaj = {
+    "voornaam" : "Kaj",
+    "Achternaam" : "Robers",
+    "Straatnaam": "Snoekbaarssingel",
+    "Postcode": "2492MH",
+    "Huisnummer": 42,
+    "Stad": "Den Haag",
+}
+print(kaj)
+
+#6.3
+print("\n 6.3 \n")
+
+woordenboek = {
+
+    "Dictionaries": "Dictionaries are used to store data values in key",
+    "List": "Lists are used to store multiple items in a single variable.",
+    "For-loop": "A for loop is used for iterating over a sequence.",
+    "String": "Strings in python are surrounded by either single quotation marks, or double quotation marks.",
+    "Integer": "Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length."
+}
+
+for x in woordenboek:
+    print(x)
