17b9e343fdbdc8b46d30283a49904f3ad2d6610c
Kaj <kaj.robers@gmail.com>
2024-02-06 13:50:57 +0100

    Wijziging bestandsnaam

--- H1 - Hello World.py
+++ H1 - Hello World.py
@@ -1,7 +1,10 @@
+#1.2
 print("Hello World") #Oorspronkelijk bericht
 print("Hello Word") #Bevat spelfout zonder error te geven
 prnt("Hello World") #Bevat spelfout die error geeft
 
 
 #1.3
-#
+#1 Programma om mijn sportdata te analyseren / goed te plotten
+#2 Programma die recepten kan zoeken o.b.v. wat er nog in de koelkast / in huis ligt
+#3
