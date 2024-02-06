343b2238d4c5ee42c2a68f39063a72a27d6434a0
Kaj <kaj.robers@gmail.com>
2024-02-06 14:18:00 +0100

    Opdrachten H2 gemaakt

---
+++ H2.py
@@ -0,0 +1,47 @@
+#2.1
+
+message = "Dit is een bericht"
+print(message)
+
+#2.2
+message = input("Nieuw bericht: ")
+print(message)
+
+#2.3
+name = "Henk "
+print("Hallo " + name + ", hoe is het?")
+
+#2.4
+print(name.lower())
+print(name.upper())
+print(name.title())
+
+#2.5
+print('J. Robert Oppenheimer once said: "I am become death, the destroyer of worlds"')
+
+#2.6
+quote = '"I am become death, the destroyer of worlds"'
+person = "J. Robert Oppenheimer"
+print(person + " once said: " + quote)
+
+#2.7
+name = "\t Piet \n \n"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+
+#2.8
+print(7+1)
+print(9-1)
+print(4*4)
+print(16/2)
+
+#2.9
+favorite_number = 42
+print("Mijn favoriete getal is: " + str(favorite_number))
+
+#2.10 -> doe ik al :)
+
+#2.11
+import this
