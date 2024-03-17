59c88475eebda7dc831986208e360a0fe34d5a17
Kaj <kaj.robers@gmail.com>
2024-03-17 14:25:24 +0100

    Toevoeging van automatisch verstelbare resolutie met een minimum van 1200x800

--- Alien Invasion/settings.py
+++ Alien Invasion/settings.py
@@ -1,8 +1,21 @@
-
+import ctypes # importeert CTypes
 class Settings():
 
     def __init__(self):
+        # Krijg de systeembreedte en hoogte
+        user = ctypes.windll.user32
+        breedte = user.GetSystemMetrics(0)
+        hoogte = user.GetSystemMetrics(1)
 
-        self.screen_width = 1200
-        self.screen_height = 800
+        # Controleer of de systeembreedte en -hoogte groter zijn dan 1200 en 800
+        if breedte > 1200 and hoogte > 800:
+            self.screen_width = breedte
+            self.screen_height = hoogte
+
+        # Gebruik de minimale  breedte en hoogte als fallback
+        else:
+            self.screen_width = 1200
+            self.screen_height = 800
+
+        # Stel de achtergrondkleur in
         self.bg_color = (0, 0, 0)
