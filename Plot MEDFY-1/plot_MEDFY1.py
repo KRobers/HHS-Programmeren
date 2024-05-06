3b03912e012a5bc14ec1b7f76ce1430a8c33748b
Kaj <kaj.robers@gmail.com>
2024-05-06 19:55:09 +0200

    Plot MEDFY Bloeddruk 1

--- Plot MEDFY-1/plot_MEDFY1.py
+++ Plot MEDFY-1/plot_MEDFY1.py
@@ -13,15 +13,19 @@ p = np.poly1d(z)
 
 
 #plot de trendlijn en data
-plt.plot(data_x, p(data_x))
-plt.scatter(data_x, data_y)
+plt.plot(data_x, p(data_x), c='grey', label='Trendlijn')
+plt.scatter(data_x, data_y, c='black', label='Meetdata')
+
+#plot van de foutmarges
+plt.errorbar(data_x,data_y, 0.01, capsize=4, linestyle='', c='black')
 
 #voeg aslabels toe
-plt.xlabel('P (mmHg)')
-plt.ylabel('U (V)')
+plt.xlabel('$\it{P}$ (mmHg)')
+plt.ylabel('$\it{U}$ (V)')
 
 #toevoegen legenda en rasterlijnen
 plt.legend()
 plt.grid(True)
 
+plt.savefig(fname='Bloeddruk_1.svg', format='svg')
 plt.show()
